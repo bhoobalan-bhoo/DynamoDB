e8a551ba86b31e0c35c5cb4a9644dcfda9b78572
Bhoobalan B R <44297926+bhoobalan-bhoo@users.noreply.github.com>
2024-02-26 11:23:10 +0530

    Update destroy.py

--- destroy.py
+++ destroy.py
@@ -16,22 +16,26 @@ dynamodb = boto3.client('dynamodb', aws_access_key_id=aws_access_key_id, aws_sec
 response = dynamodb.describe_table(TableName=table_name)
 key_attributes = [key['AttributeName'] for key in response['Table']['KeySchema']]
 
-# Scan and delete all items in the t    able
+# Scan and delete all items in the table using transact_write_items
 def delete_all_items():
     response = dynamodb.scan(TableName=table_name)
 
+    items_to_delete = []
+
     for item in response['Items']:
         key = {k: v for k, v in item.items() if k in key_attributes}
-        dynamodb.delete_item(TableName=table_name, Key=key)
-        print(f"Deleted item: {key}")
+        items_to_delete.append({'Delete': {'TableName': table_name, 'Key': key}})
+        print(f"Queued item for deletion: {key}")
 
-    while 'LastEvaluatedKey' in response:
-        response = dynamodb.scan(TableName=table_name, ExclusiveStartKey=response['LastEvaluatedKey'])
+    # Batch write to delete items using transact_write_items
+    while items_to_delete:
+        batch_size = min(25, len(items_to_delete))  # transact_write_items has a limit of 25 items per request
+        batch_items, items_to_delete = items_to_delete[:batch_size], items_to_delete[batch_size:]
 
-        for item in response['Items']:
-            key = {k: v for k, v in item.items() if k in key_attributes}
-            dynamodb.delete_item(TableName=table_name, Key=key)
-            print(f"Deleted item: {key}")
+        transact_request = [{'Delete': item['Delete']} for item in batch_items]
+        dynamodb.transact_write_items(TransactItems=transact_request)
+
+        print(f"Deleted {batch_size} items")
 
 if __name__ == '__main__':
     delete_all_items()
